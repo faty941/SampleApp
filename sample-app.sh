c59559ee80f97363c16f52eb32e362fbb148a1f5
faty941 <140063346+faty941@users.noreply.github.com>
2026-08-14 17:36:28 +0000

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -8,7 +8,7 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
+echo "FROM python:3.11" >> tempdir/Dockerfile
 echo "RUN pip install --no-cache-dir --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
